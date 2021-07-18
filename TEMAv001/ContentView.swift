35fc2eb61090cf08115c15f3c9387ebf8741f39d
NeoTEo <m@sartori.dk>
2021-07-18 10:13:53 +0200

    Initial Commit

---
+++ TEMAv001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TEMAv001
+//
+//  Created by teo on 18/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TEMAv001/TEMAv001App.swift
@@ -0,0 +1,17 @@
+//
+//  TEMAv001App.swift
+//  TEMAv001
+//
+//  Created by teo on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TEMAv001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
